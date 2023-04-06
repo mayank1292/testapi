d7cb1c7b5aa5249746ab227ee65edbd5852fae16
mayank1292 <mayank.1292@gmail.com>
2023-04-07 04:55:31 +0530

    initial commit

---
+++ src/main/java/com/camtest/backend/testapi/controller/AllControllers.java
@@ -0,0 +1,53 @@
+/*
+ * $Id$
+ *
+ * Copyright &copy; 1999-2022, i:FAO AG. All rights reserved.
+ * www.ifao.net
+ */
+
+
+package com.camtest.backend.testapi.controller;
+
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Class AllControllers
+ */
+@RestController
+@RequestMapping("/api/v1/jaxrsclient")
+public class AllControllers
+{
+
+    @GetMapping("/users")
+    @ResponseBody
+    public String getUsers(HttpServletResponse response)
+    {
+        response.setContentType("application/json");
+
+        String usersJson =
+            "[{\"name\" : \"sesi\", \"email\" :\"sesi@amadeus.com\"},{\"name\" : \"rekha\", \"email\" :\"rekha@amadeus.com\"},{\"name\" : \"sesirekha\", \"email\" :\"sesirekha@amadeus.com\"}]";
+
+        return usersJson;
+    }
+    
+    @GetMapping("/systems")
+    @ResponseBody
+    public String getSystems(HttpServletResponse response)
+    {
+        response.setContentType("application/json");
+
+        String systems = 
+        		"[{\"corporate_id\":\"874568\",\"corporatecode\":\"TestSystem\",\"parent_id\":\"852567\"},{\"corporate_id\":\"877768\",\"corporatecode\":\"First\",\"parent_id\":\"852567\"},{\"corporate_id\":\"8775868\",\"corporatecode\":\"Bangalore\",\"parent_id\":\"8528567\"},{\"corporate_id\":\"8774868\",\"corporatecode\":\"QASystem\",\"parent_id\":\"8528567\"},{\"corporate_id\":\"8854868\",\"corporatecode\":\"Hotel\",\"parent_id\":\"8528567\"},{\"corporate_id\":\"8854868\",\"corporatecode\":\"Air\",\"parent_id\":\"8528567\"}]";
+
+        return systems;
+    }
+
+
+}
---
+++ src/test/java/com/camtest/backend/testapi/TestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camtest.backend.testapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
